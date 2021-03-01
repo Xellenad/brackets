b91eaa808599e552d4bdb729593e440a01570c42
Xellenad <Xellenad54@gmail.com>
2021-03-01 10:45:57 +0300

    complite task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let leftArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+      if (str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '|') {
+          leftArr.push(str[i]);
+      }
+
+      let leftArrLength = leftArr.length;
+
+      if (str[i] === ')' && leftArr[leftArrLength - 1] === '(') {
+          leftArr.pop();
+      } else if (str[i] === '}' && leftArr[leftArrLength - 1] === '{') {
+          leftArr.pop();
+      } else if (str[i] === ']' && leftArr[leftArrLength - 1] === '[') {
+          leftArr.pop();
+      }  else if (str[i] === '|' && leftArr[leftArrLength - 1] === '|') {
+        leftArr.pop();
+      } else if (str[i] === ')' || str[i] === '}' || str[i] === ']' || str[i] === '|') {
+        return false;
+      }
+  }
+  return leftArr.length === 0;
 }
+
